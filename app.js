4fcc01e9b48e6fc51fe320a952e7a1a9d373e6af
shelz09 <chaudharyshlesh1109@gmail.com>
2024-12-27 21:18:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const button = document.addEventListener('click', () => {
+    console.log("hii azam")
+});
+const slides = document.querySelectorAll(".slide");
+console.log(slides);
+let counter = 0;
+slides.forEach(
+    (slide,index) => {
+        slide.style.left=`${index*100}%`
+    }
+) 
+const  SlideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter*100}%)`
+        }
+    )
+}
+const GoPrev = () => {
+    if(counter!=0) {
+        counter--;
+        SlideImage();
+    }
+}
+const GoNext = () => {
+    if(counter < slides.length - 1) {
+        counter++;
+        SlideImage();
+    }
+}
